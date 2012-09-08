27c9ac88eb56fa3baea7a903f0be3d7eac0471d0
Scott Schlangen <sschlangen@fullsail.edu>
2012-09-08 10:48:28 -0400

    Added Files to SDI Project 2 Directory

---
+++ sdiproject2/SchlangenS_Project2.js
@@ -0,0 +1,51 @@
+//Scott A Schlangen
+//SDI 1209
+//Project 2
+//Functions and Loops
+
+
+
+//Variables --> String,Number,Array
+
+
+
+
+
+
+
+
+//Procedure --> Argument, Argument Conditional (Output for both true, and false) return back
+
+
+
+
+//Boolean Function --> Two Arguments, Compairions, OUTPUT (true and False, Return Boolean
+
+
+
+
+
+//Number Function --> Pass number, Local Variable within number functions, while loop (if false return number, if true, Math to output and return number
+
+
+
+
+
+//String Function --> Pass two strings, local variables, String Concatenation, Return String
+
+
+
+
+
+
+
+
+//Array Function  --> Number and Array passed through, Local Variables, For loop if true Math and the output, if false return array
+
+
+
+
+
+
+//Main Code - Function Calls
+    
