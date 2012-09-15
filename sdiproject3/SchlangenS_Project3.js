051d87496aedf949900bf978c936e0ffbea4f885
Scott Schlangen <sschlangen@fullsail.edu>
2012-09-14 22:50:24 -0400

    Changed back to my original story
    Changed back to original story and I'm stuck

--- sdiproject3/SchlangenS_Project3.js
+++ sdiproject3/SchlangenS_Project3.js
@@ -1,51 +1,110 @@
 //Scott A Schlangen
 //SDI 1209
-//Project 3
-//9-11 from my perspective
+//Project 2
 
-//variables
-var teacher = "Mr Hurt",
-    numberofStudents = 25,
-    time = "08:46AM",
-    planeHit = true,
-    place = "Bloomington North High School, Indiana Algebra Class",
-    cryingGirl = "Katy"
-    peopleNames = [ "Myself", "Katy", "Hillary" ],
-    tvChannels = {
-        channel11: "Fox 59",
-        channel3: "WTHR",
-        channel4: "CNN",
-        channel8: "Wish TV"
+
+
+
+
+
+//Variables --> String,Number,Array
+
+var	samWater = "My wifes water just broke",
+	samPainRate = 10,
+	orderOfAction = ["get in the car", "drive the speed of light", "arrive at the hospital", "baby is born"];
+	
+	
+	
+
+//JSON Information
+var daysJSON = {
+    "day": [
+        {
+            "day": "Monday",
+            "baby": "Amelia",
+            "action": "ate three ounces",
+            "temp": "and her temperatue was appropriate"
+        },
+        {
+           	"day": "Tuesday",
+            "baby": "Amelia",
+            "action": "ate three ounces",
+            "temp": "and her temperatue was appropriate"
+        },
+        {
+            "day": "Wednesday",
+            "baby": "Amelia",
+            "action": "ate three ounces",
+            "temp": "and her temperatue was appropriate"
+        },
+        {
+	        "day": "Thursday",
+            "baby": "Amelia",
+            "action": "ate three ounces",
+            "temp": "and her temperatue was appropriate"
+        },
+        {
+         	"day": "Friday",
+            "baby": "Amelia",
+            "action": "ate three ounces",
+            "temp": "and her temperatue was appropriate"
+        }    
+    ]
+};
+	
+
+
+//Procedure --> Argument, Argument Conditional (Output for both true, and false) return back
+
+var waterBreaks = function(water){   
+	if (water == "My wifes water just broke"){  
+console.log( water + ", and we are on our way to the hospital");    
+} else {    
+console.log ("We are sleeping in our bed getting our beauty rest");   
+}   
 };
 
 
 
-var begin = {
-					//Procedure
-	 goingToAlgebra : function (mathTeacher) {
-                        if ( mathTeacher == "Mr Hurt") {
-                            console.log("I was heading to " + teacher + "'s " + "Algebra Class to start period 2.")
-                        } else {
-                            console.log("I was still in 1st period class");
-                            goingToAlgebra("Mr Hurt");
-                        }
-                
-       },
-       
-       				//Boolean Function | Was it an accident
-       
-       accidental : function (planeTerror,girl){
-	       if (planTerror = true && girl == "Katy"){
-		       console.log ("This was no accident" + "," + "and Katy my classmate becomes hysterical and starts crying." + "I can do nothing about the situation and comfort my fellow class mates.");
-		           accidental = "false";
-		      } else {
-			       console.log ("The planes never hit on purpose, and it was not a terroist activity.");
-			       accidental = "true";
-			       
-		       }
-		       return accidental;
-	       }     //end accidental function  
-       };      //end begin object
+//Boolean Function --> Two Arguments, Compairions, OUTPUT (true and False, Return Boolean
+
+var stepOne = function(samWater, samPainRate){
+	if (samWater == "My wifes water just broke" && samPainRate >= 10){
+		headingToHospital = true;
+		console.log ("We are going to " + orderOfAction[0] + ", and " + orderOfAction[1]);
+	} else{
+		headingToHospital = false;
+		console.log ("We are going to just keep sleeping in bed, and have our beauty sleep");
+	}
+	return headingToHospital
+};
+
+
+
+
+
+//Number Function --> Pass number, Local Variable within number functions, while loop (if false return number, if true, Math to output and return number
+
+var stepTwo = function(samPainRate){
+	while (samPainRate <= 15){
+	console.log( "My wife's pain rate is climbing at " + samPainRate + ", and she needs some pain medicine");
+	samPainRate++;
+	};
+	return samPainRate;
+};
+
+
+
+
+
+//String Function --> Pass two strings, local variables, String Concatenation, Return String
+
+var stepThree = function (orderOfAction){
+	hosp = "Community South";
+	floor = "3rd Floor";
+	console.log( "We " + orderOfAction[2] + " of " + hosp + " assigned to the " + floor + ". We are super excited to get medicine for pain, and witness the birth of our first baby!!");
+return hosp;
+};
 
 
 
@@ -54,12 +113,43 @@ var begin = {
 
 
 
+//Array Function  --> Number and Array passed through, Local Variables, For loop if true Math and the output, if false return array
+var babyNames = function(){
+	girlNames = ["Amelia Jane", "Emma Jane", "Catherine Jane"];
+	boyNames  = ["Craig Matthew", "Travis Scott", "Arthur Charles"];
+	
+	
+			for (var i=0, j=boyNames.length; i<j; i++){
+			console.log("If our baby was a boy, we thought about the names " +boyNames[i]);
+			};
+				
+			for (var i=0, j=girlNames.length; i<j; i++){
+			console.log("Since our baby is a girl, we thought about the name " +girlNames[i]);
+			};
+return girlNames[0];
+};
 
 
 
 
-//Calls Telling the Story
+ //Accessor Method: Sequence of events from the json
+var bringBabyHome = {
+   	 dayHandler: function(json){
+            for (var i = 0; i < json.day; i++) {
+            whatHappend = json.day[i];
+	        console.log("This is exactly how our first week played out");
+	        console.log("On" + day.day + day.baby + day.action + day.temp);
+            return whatHappend;
+}
 
+ 
 
-begin.goingToAlgebra(teacher);
-begin.accidental(planeHit,cryingGirl);
+//Main Code - Function Calls
+
+waterBreaks(samWater);
+stepOne(samWater, samPainRate);
+stepTwo(samPainRate);
+stepThree(orderOfAction);    
+babyNames();
+console.log("Our baby's name is " + girlNames[0]);
+bringBabyHome.dayHandler(daysJSON);
