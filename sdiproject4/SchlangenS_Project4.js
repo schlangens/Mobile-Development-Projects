2c733451c0ecc2d3060e703fcc0bb9abef3eebe5
Scott Schlangen <sschlangen@fullsail.edu>
2012-09-20 11:50:25 -0400

    Number Function # 1 added
    Added number function #1 of 6
    
    What it does:
    Finds the number of hours or days difference between two dates
    Variable:
    checkIt

--- sdiproject4/SchlangenS_Project4.js
+++ sdiproject4/SchlangenS_Project4.js
@@ -1,1 +1,38 @@
-alert("JavaScript works!");
+//Scott A Schlangen
+//SDI 1209
+//Project 4
+
+
+    
+
+//Number Section
+
+/*Find the number of hours or days difference between two dates.*/
+
+   var checkIt = function (firstDate, secondDate) { 
+        oneDay = 1000*60*60*24;
+        oneHour = 1000*60*60;
+        if (firstDate < secondDate) {
+            dayDif = Math.ceil((secondDate.getTime()-firstDate.getTime())/(oneDay));
+            hourDif = Math.ceil((secondDate.getTime()-firstDate.getTime())/(oneHour));
+        } else if (firstDate > secondDate) {
+            dayDif = Math.ceil((firstDate.getTime()-secondDate.getTime())/(oneDay));
+            hourDif = Math.ceil((firstDate.getTime()-secondDate.getTime())/(oneHour));
+        }
+        return [dayDif, hourDif];
+    }; //close checkIt Function
+    
+    
+    
+    
+    
+    
+    
+ 
+    
+
+    
+//Calls
+
+checkIt(new Date(2011, 10, 1), new Date()); //Last year birthday YYYY, MM, DD 
+console.log("Days: " + dayDif + ";  hours: " + hourDif + ";");    
